e5c0878afdef974c17a3dddfbf9464766acc23d1
ppranav06 <pranavkrishnap856@gmail.com>
2023-12-04 00:36:02 +0530

    Fixed withdraw, deposit, transfer methods and menu

--- banking_application(standalone).py
+++ banking_application(standalone).py
@@ -1,44 +1,45 @@
 #!/usr/bin/env python3
 
 # Banking application using python - implemented via dictionaries
-'''
-import csv
-import finance_management,account_management
-'''
 
 accounts={
-    0000000000000:{
+    # Format: 10000|570|00000 - Bank|Branch|AccountNumber
+    1000057000000:{
         'name':str(),
         'balance':float()
     }
 }
 
 errorDict={
-    'INVALIDERR':'Invalid Account Number',
-    'ABSENTERR':'Account not found',
-    'EXISTERR':'Account already exists',
-    'BALANCEERR':'Insufficient Balance',
-    'MAXDEPOSIT':'Maximum Deposit Limit is 100000',
-    'INPUTERR':'Invalid Input Given',
+    'INVALID_ERR':'Invalid Account Number',
+    'ABSENT_ERR':'Account not found',
+    'EXIST_ERR':'Account already exists',
+    'INSUF_FUNDS_ERR':'Insufficient Balance',
+    'MIN_BALANCE_ERR':'Minimum Balance not maintained',
+    'MAXLIMIT_ERR':'Maximum Deposit Limit is 100000',
+    'INPUT_ERR':'Invalid Input Given',
 }
 printerror = lambda string: print(f"{string}: {errorDict[string]}")
 
-def enterKeyToContinue():
+def press_enter_to_continue():
     input("Press Enter to continue...")
+def account_number_validity(accountNumber):
+    return (len(str(accountNumber))==13) and ('10000570' in str(accountNumber))
+
+
 # -------------------------------------------------------------------
 # ACCOUNT MANAGEMENT METHODS
 # -------------------------------------------------------------------
 
-def checkAccount(accountNumber):
+def accountExists(accountNumber):
     if accountNumber in accounts:
         return True
     
     return False
 
 def createAccount(accountNumber,name='',initialBalance=1000):
-    if checkAccount(accountNumber):
-        printerror('EXISTERR')
-        
+    if accountExists(accountNumber):
+        printerror('EXIST_ERR')
         return
     
     # Gathering details and appending to accounts dict database
@@ -48,14 +49,15 @@ def createAccount(accountNumber,name='',initialBalance=1000):
 
     # Add the account to dict database
     accounts[accountNumber]={'name': name, 'balance':initialBalance}
-    print("Addition successful") if checkAccount(accountNumber) else print("error in adding account")
+    print("Addition successful") if accountExists(accountNumber) else print("error in adding account")
     
 
 def retreiveDetails(accountNumber):
-    if not checkAccount(accountNumber):
-        printerror('ABSENTERR')
+    if not accountExists(accountNumber):
+        printerror('ABSENT_ERR')
         return
     
+    # Gather the name and available balance of account number
     details=accounts[accountNumber]
     print(f"Details of the account holder for {accountNumber}:")
     print(f"Name of the Holder: {details['name']} \nBalance: {details['balance']}")
@@ -66,13 +68,12 @@ def retreiveDetails(accountNumber):
 # -------------------------------------------------------------------
 
 def validatePAN(pan): 
-    if len(pan)==10 and pan.isalnum(): return True
-    else: return False
+    return len(pan) == 10 and pan.isalnum()
 
 def deposit(accountNumber, amount):
     # Constraints
-    if not checkAccount(accountNumber):
-        printerror('ABSENTERR')
+    if not accountExists(accountNumber):
+        printerror('ABSENT_ERR')
         return
 
     if amount>=100000:
@@ -87,13 +88,22 @@ def deposit(accountNumber, amount):
     
 def withdraw(accountNumber, amount):
     # Constraints
-    if not checkAccount(accountNumber):
-        printerror('ABSENTERR')
+    if not accountExists(accountNumber):
+        printerror('ABSENT_ERR')
         return
 
-    balance=accounts[accountNumber]['balance']
-    if (amount-balance) < 1000:
-        printerror('BALANCEERR')
+    existing=accounts[accountNumber]['balance']
+    
+    # withdraw past available money prevention - insufficient funds
+    if amount > existing:
+        printerror('INSUF_FUNDS_ERR')
+        print("Transaction Failed")
+        return
+
+    # withdraw prevention if balance < 1000
+    if (amount-existing) < 1000:
+        printerror('MIN_BALANCE_ERR')
+        print("Transaction Failed")
         return
 
     # Withdrawal - remove the amount from balance
@@ -101,22 +111,28 @@ def withdraw(accountNumber, amount):
     print(f"Withdrawal of {amount} successful")
     
 
-def transfer(fromAcc, toAcc, amount=0):
+def transfer(fromAcc, toAcc, amount):
     # Withdraws fromAcc and Deposits toAcc - all constraints followed
-    
-    if (amount-accounts[fromAcc]['balance']) < 1000:
-        printerror('BALANCEERR')
+
+    if fromAcc not in accounts or toAcc not in accounts:
+        printerror('ABSENT_ERR')
+        print("Atleast one of the account numbers doesn't exist.")
         return
     
-    if amount>=100000:
-        printerror('MAXDEPOSIT')
+    if (amount - accounts[fromAcc]['balance']) < 1000:
+        printerror('BALANCE_ERR')
+        return
+    
+    if (amount + accounts[toAcc]['balance']) >= 100000:
+        printerror('MAXLIMIT_ERR')
         pan=input("Enter PAN: ")
         if validatePAN(pan)==False: 
-            printerror('PANERR')
+            printerror('PAN_ERR')
             return
 
     accounts[fromAcc]['balance'] -= amount  #withdraw(fromAcc, amount)
     accounts[toAcc]['balance'] += amount    #deposit(toAcc,amount)
+    print(f"Transfer of {amount} from {fromAcc} to {toAcc} successful.")
 
 # -------------------------------------------------------------------
 # Main Code starts here
@@ -124,52 +140,54 @@ def transfer(fromAcc, toAcc, amount=0):
 
 menu="""
 BANKING PORTAL
---ACCOUNT
-1] Add a new Account
-2] Retreive Account Details
 --FINANCE
-3] Deposit Amount to Account
-4] Withdraw Amount from Account
-5] Transfer Amount
-Choice: (0 to exit)
+1] Withdraw Amount from Account
+2] Deposit Amount to Account
+3] Transfer Amount
+--ACCOUNT
+4] Retreive Account Details
+
+Choice: (0 to logout)
 """
 while True:
+    accountNumber=int(input("Account Number: "))
+    # the 
+    if not accountExists(accountNumber):
+        printerror('ABSENT_ERR')
+        continue
+    if not account_number_validity(accountNumber):
+        printerror('INVALID_ERR')
+        continue
+# FUNCTIONALITY TO ADD:
+# CREATE AN ACCOUNT FROM MENU SOMEHOW
     choice=int(input(menu))
+    # switch-case statement to implement menu-driven portal
     match choice:
         case 1:
-            accountNumber=int(input("Account Number to Add: "))
-            createAccount(accountNumber)
-            enterKeyToContinue()
-        
-        case 2:
-            accountNumber=int(input("Account Number: "))
-            retreiveDetails(accountNumber)
-            enterKeyToContinue()
-
-        case 3:
-            accountNumber=int(input("Account Number: "))
-            amount=float(input("Amount to Deposit: "))
-            deposit(accountNumber, amount)
-            enterKeyToContinue()
-
-        case 4:
-            accountNumber=int(input("Account Number: "))
             amount=float(input("Amount to Withdraw: "))
             withdraw(accountNumber, amount)
-            enterKeyToContinue()
+            press_enter_to_continue()
 
-        case 5:
-            accNum1=int(input("Transfer from: "))
+        case 2:
+            amount=float(input("Amount to Deposit: "))
+            deposit(accountNumber, amount)
+            press_enter_to_continue()
+
+        case 3:
             accNum2=int(input("Transfer to: "))
             amount=int(input("Amount to transfer: "))
-            transfer(accNum1,accNum2,amount)
-            enterKeyToContinue()
-        
+            transfer(accountNumber,accNum2,amount)
+            press_enter_to_continue()
+
+        case 4:
+            retreiveDetails(accountNumber)
+            press_enter_to_continue()
+     
         case 0:
-            # END
+            # END 
             break
         
         case _:
-            printerror('INPUTERR')
-            enterKeyToContinue()
+            printerror('INPUT_ERR')
+            press_enter_to_continue()
         
